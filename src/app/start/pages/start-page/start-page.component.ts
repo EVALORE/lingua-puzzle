c6c547fd7e61f6e5894aeaa7982a430a23ddf4be
Vladimir Eremia <vladimireremia377@gmail.com>
2024-09-09 17:54:04 +0300

    feat: add start screen content (#5)

--- src/app/start/pages/start-page/start-page.component.ts
+++ src/app/start/pages/start-page/start-page.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-start-page',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './start-page.component.html',
   styleUrl: './start-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
