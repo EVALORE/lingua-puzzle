ad01c529e75f935560525101916f51e5617e67ee
Vladimir Eremia <caioshi212@gmail.com>
2024-09-16 10:34:42 +0300

    feat: replace blocks components into main-page

--- src/app/lingua-puzzle/pages/main-page/main-page.component.ts
+++ src/app/lingua-puzzle/pages/main-page/main-page.component.ts
@@ -1,13 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { SourceBlockComponent } from '../../components/source-block/source-block.component';
-import { ResultBlockComponent } from '../../components/result-block/result-block.component';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { GameService } from '../../services/game.service';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [SourceBlockComponent, ResultBlockComponent],
+  imports: [MatCard],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainPageComponent {}
+export class MainPageComponent {
+  private readonly gameService = inject(GameService);
+  protected source = this.gameService.source;
+  protected result = this.gameService.result;
+
+  public onWordClick($index: number, target: 'source' | 'result'): void {
+    if (target === 'source') {
+      this.gameService.moveToSource($index);
+    }
+    if (target === 'result') {
+      this.gameService.moveToResult($index);
+    }
+  }
+}
