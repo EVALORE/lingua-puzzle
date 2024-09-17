d901041813af7bd22a25b745b337c11f6636a5e6
Vladimir Eremia <caioshi212@gmail.com>
2024-09-17 10:48:41 +0300

    feat: move flip animation logic into a service

--- src/app/lingua-puzzle/components/word-card/word-card.component.ts
+++ src/app/lingua-puzzle/components/word-card/word-card.component.ts
@@ -8,23 +8,10 @@ import {
   output,
 } from '@angular/core';
 import { MatCard } from '@angular/material/card';
+import { FlipAnimationService } from '../../../shared/services/flip-animation.service';
 
 export interface Card {
   word: string;
-  replace: boolean;
-  id?: number;
-  first?: {
-    top: number;
-    left: number;
-    width: number;
-    height: number;
-  };
-  last?: {
-    top: number;
-    left: number;
-    width: number;
-    height: number;
-  };
 }
 
 @Component({
@@ -37,51 +24,20 @@ export interface Card {
 })
 export class WordCardComponent implements AfterViewInit {
   public card = input.required<Card>();
-
   public onClick = output();
 
+  private readonly fas = inject(FlipAnimationService);
   private elementRef = inject<ElementRef<HTMLElement>>(ElementRef);
 
   public emitClick(): void {
     const componentElement = this.elementRef.nativeElement;
-    const { top, left, width, height } = componentElement.getBoundingClientRect();
-    this.card().first = { top, left, width, height };
+    this.fas.updateFirst(componentElement.getBoundingClientRect());
     this.onClick.emit();
   }
 
   public ngAfterViewInit(): void {
     const componentElement = this.elementRef.nativeElement;
-
-    const card = this.card();
-
-    const { top, left, width, height } = componentElement.getBoundingClientRect();
-    card.last = { top, left, width, height };
-
-    if (card.first) {
-      const { first, last } = card;
-      const deltaX = (first.left - last.left).toString();
-      const deltaY = (first.top - last.top).toString();
-      const deltaW = (first.width / last.width).toString();
-      const deltaH = (first.height / last.height).toString();
-      componentElement.animate(
-        [
-          {
-            transformOrigin: 'top left',
-            transform: `
-              translate(${deltaX}px, ${deltaY}px)
-              scale(${deltaW}, ${deltaH})`,
-          },
-          {
-            transformOrigin: 'top left',
-            transform: 'none',
-          },
-        ],
-        {
-          duration: 400,
-          easing: 'ease-in-out',
-          fill: 'both',
-        },
-      );
-    }
+    this.fas.updateLast(componentElement.getBoundingClientRect());
+    this.fas.proceedAnimation(componentElement);
   }
 }
