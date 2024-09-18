f37b9b51ce0dd68a14add2784a1695e201d24dcf
Vladimir Eremia <caioshi212@gmail.com>
2024-09-18 16:18:07 +0300

    fix: add sentenceLength property

--- src/app/lingua-puzzle/components/game/game.component.ts
+++ src/app/lingua-puzzle/components/game/game.component.ts
@@ -22,6 +22,7 @@ import { WordCardDirective } from '../../directives/word-card.directive';
 })
 export class GameComponent {
   private readonly gameService = inject(GameService);
+  protected sentenceLength = this.gameService.charInSentence;
   protected source = this.gameService.source;
   protected result = this.gameService.result;
 
