1b1ce02304fdf834929e9679c57eae189d9a7bd4
Vladimir Eremia <caioshi212@gmail.com>
2024-09-19 21:02:50 +0300

    refactor(animation): extract animation logic into separate function

--- src/app/lingua-puzzle/components/game/game.component.ts
+++ src/app/lingua-puzzle/components/game/game.component.ts
@@ -1,9 +1,22 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import {
+  animate,
+  AnimationTransitionMetadata,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatCard } from '@angular/material/card';
 import { WordCardDirective } from '../../directives/word-card.directive';
 import { Card, GameService } from '../../services/game.service';
 import { MatButton } from '@angular/material/button';
+
+function gapCollapseAnimation(): AnimationTransitionMetadata {
+  return transition(':leave', [
+    style({ opacity: 0 }),
+    animate('600ms ease-in', style({ width: 0, padding: 0 })),
+  ]);
+}
 
 @Component({
   selector: 'app-game',
@@ -12,14 +25,7 @@ import { MatButton } from '@angular/material/button';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [
-    trigger('filterAnimation', [
-      transition(':leave', [
-        style({ opacity: 0 }),
-        animate('600ms ease-in', style({ width: 0, padding: 0 })),
-      ]),
-    ]),
-  ],
+  animations: [trigger('filterAnimation', [gapCollapseAnimation()])],
 })
 export class GameComponent {
   private readonly gameService = inject(GameService);
