db721ec19db32bddd2c65e46fcbe3fae9e314ecf
Vladimir Eremia <caioshi212@gmail.com>
2024-09-25 17:09:57 +0300

    refactor(game.component): implement puzzle.component

--- src/app/lingua-puzzle/components/game/game.component.ts
+++ src/app/lingua-puzzle/components/game/game.component.ts
@@ -5,21 +5,12 @@ import {
   transition,
   trigger,
 } from '@angular/animations';
-import { ChangeDetectionStrategy, Component, effect, inject } from '@angular/core';
-import { MatCard } from '@angular/material/card';
-import { WordCardDirective } from '../../directives/word-card.directive';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { GameService } from '../../services/game.service';
 import { MatButton } from '@angular/material/button';
-import { Card } from '../../../shared/types/card.interface';
-import {
-  CdkDrag,
-  CdkDragDrop,
-  CdkDropList,
-  moveItemInArray,
-  transferArrayItem,
-} from '@angular/cdk/drag-drop';
 import { HintsComponent } from '../hints/hints.component';
 import { CardListComponent } from '../card-list/card-list.component';
+import { PuzzleComponent } from '../puzzle/puzzle.component';
 
 function gapCollapseAnimation(): AnimationTransitionMetadata {
   return transition(':leave', [
@@ -31,15 +22,7 @@ function gapCollapseAnimation(): AnimationTransitionMetadata {
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [
-    MatCard,
-    WordCardDirective,
-    MatButton,
-    CdkDrag,
-    CdkDropList,
-    HintsComponent,
-    CardListComponent,
-  ],
+  imports: [MatButton, HintsComponent, CardListComponent, PuzzleComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -47,29 +30,10 @@ function gapCollapseAnimation(): AnimationTransitionMetadata {
 })
 export class GameComponent {
   private readonly gameService = inject(GameService);
-
-  protected source = this.gameService.source;
-  protected result = this.gameService.result;
   protected isWin = this.gameService.isWin;
-  protected completedSentences: Card[][] = [];
-  protected picture = '';
-
-  constructor() {
-    effect(() => {
-      this.picture = `project-data/images/${this.gameService.picture().imageSrc}`;
-    });
-  }
 
   protected startResultAutoComplete(): void {
     this.gameService.sortCardsInCorrectOrder();
-  }
-
-  protected moveToSource(wordIndex: number): void {
-    this.gameService.moveToSource(wordIndex);
-  }
-
-  protected moveToResult(wordIndex: number): void {
-    this.gameService.moveToResult(wordIndex);
   }
 
   protected nextSentence(): void {
@@ -83,18 +47,5 @@ export class GameComponent {
 
   private pushSentenceToCompleted(): void {
     this.completedSentences.push(this.result());
-  }
-
-  protected drop(event: CdkDragDrop<Card[]>): void {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-    }
   }
 }
