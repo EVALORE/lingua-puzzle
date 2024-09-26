0527a830eba2fe610ebdd372c651c16652d63709
Vladimir Eremia <caioshi212@gmail.com>
2024-09-26 10:43:19 +0300

    refactor(round.service): remove unused properties

--- src/app/lingua-puzzle/services/round/round.service.ts
+++ src/app/lingua-puzzle/services/round/round.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable, signal } from '@angular/core';
-import { Picture, Round, Sentence } from '../../../shared/types/http-data.interface';
+import { Round, Sentence } from '../../../shared/types/http-data.interface';
 import { HttpDataService } from '../../../core/services/http-data.service';
 import { PictureService } from '../picture/picture.service';
 
@@ -8,12 +8,12 @@ import { PictureService } from '../picture/picture.service';
 })
 export class RoundService {
   private readonly pictureService = inject(PictureService);
-  public round = signal({} as Round);
   private readonly httpData = inject(HttpDataService);
+
+  public rounds: Round[] = [];
   public roundIndex = 0;
 
   public sentence = signal({} as Sentence);
-  public picture = signal({} as Picture);
 
   constructor() {
     this.setRound();
@@ -21,6 +21,7 @@ export class RoundService {
 
   public setRound(): void {
     this.httpData.getRounds().subscribe((rounds) => {
+      this.rounds = rounds;
       this.pictureService.setPicture(rounds[this.roundIndex].levelData);
       this.sentence.set(rounds[this.roundIndex].words[0]);
     });
