7b795f7a9510d38e4b1e1d887104bbcb6928e736
Vladimir Eremia <caioshi212@gmail.com>
2024-09-30 14:36:37 +0300

    feat(hint-settings): implement hints settings functionality

--- src/app/lingua-puzzle/components/game/game.component.ts
+++ src/app/lingua-puzzle/components/game/game.component.ts
@@ -1,19 +1,24 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { HintsComponent } from '../hints/hints.component';
 import { PuzzleComponent } from '../puzzle/puzzle.component';
 import { PuzzleService } from '../../services/puzzle/puzzle.service';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [MatButton, HintsComponent, PuzzleComponent],
+  imports: [MatButton, HintsComponent, PuzzleComponent, MatCheckbox, FormsModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GameComponent {
   private readonly puzzleService = inject(PuzzleService);
+  public readonly enableTranslation = signal(true);
+  public readonly enableAudio = signal(true);
+  public readonly enablePicture = signal(true);
   protected isSourceEmpty = this.puzzleService.isSourceEmpty;
   protected isWin = this.puzzleService.arePositionsCorrect;
 
@@ -22,7 +27,7 @@ export class GameComponent {
   }
 
   protected nextSentence(): void {
-    this.puzzleService.nextSentence()
+    this.puzzleService.nextSentence();
   }
 
   protected checkResultCorrectness(): void {
