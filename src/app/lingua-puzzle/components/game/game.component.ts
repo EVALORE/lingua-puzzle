eeccbbdea8e6bc1c9dc8f2b9e14827d7858388a1
Vladimir Eremia <caioshi212@gmail.com>
2024-10-01 13:49:10 +0300

    feat(hint settings): implement storing of hints settings

--- src/app/lingua-puzzle/components/game/game.component.ts
+++ src/app/lingua-puzzle/components/game/game.component.ts
@@ -1,10 +1,17 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, OnInit, signal } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { HintsComponent } from '../hints/hints.component';
 import { PuzzleComponent } from '../puzzle/puzzle.component';
 import { PuzzleService } from '../../services/puzzle/puzzle.service';
 import { MatCheckbox } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
+import { LocalStorageService } from '../../../core/storage/services/local-storage/local-storage.service';
+
+interface hintsSettingStorage {
+  translation: boolean;
+  audio: boolean;
+  picture: boolean;
+}
 
 @Component({
   selector: 'app-game',
@@ -14,13 +21,27 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './game.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  private readonly localStorage = inject(LocalStorageService);
+  public readonly hintsSetting = {
+    translation: signal<boolean>(this.getSettingFromStorage().translation),
+    audio: signal<boolean>(this.getSettingFromStorage().audio),
+    picture: signal<boolean>(this.getSettingFromStorage().picture),
+  };
+
   private readonly puzzleService = inject(PuzzleService);
-  public readonly enableTranslation = signal(true);
-  public readonly enableAudio = signal(true);
-  public readonly enablePicture = signal(true);
   protected isSourceEmpty = this.puzzleService.isSourceEmpty;
   protected isWin = this.puzzleService.arePositionsCorrect;
+
+  constructor() {
+    effect(() => {
+      this.localStorage.setItem('hintsSetting', {
+        translation: this.hintsSetting.translation(),
+        audio: this.hintsSetting.audio(),
+        picture: this.hintsSetting.picture(),
+      });
+    });
+  }
 
   protected startResultAutoComplete(): void {
     this.puzzleService.sortCardsByOriginalIndex();
@@ -32,5 +53,22 @@ export class GameComponent {
 
   protected checkResultCorrectness(): void {
     this.puzzleService.updateCardsPositionStatus();
+  }
+
+  private getSettingFromStorage(): hintsSettingStorage {
+    const defaultSettings = {
+      translation: true,
+      picture: true,
+      audio: true,
+    };
+
+    return this.localStorage.getItem<hintsSettingStorage>(
+      'hintsSetting',
+      defaultSettings,
+    ) as hintsSettingStorage;
+  }
+
+  public ngOnInit(): void {
+    this.getSettingFromStorage();
   }
 }
