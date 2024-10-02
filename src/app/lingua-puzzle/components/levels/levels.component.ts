65a69c737fb6c46c680aab1c7a0f66c9910f4b39
Vladimir Eremia <caioshi212@gmail.com>
2024-10-02 14:43:31 +0300

    feat(levels.component): implement round selection

--- src/app/lingua-puzzle/components/levels/levels.component.ts
+++ src/app/lingua-puzzle/components/levels/levels.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 import {
   MatFormField,
   MatLabel,
@@ -8,6 +8,7 @@ import {
 } from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
 import { LevelService } from '../../services/level/level.service';
+import { RoundService } from '../../services/round/round.service';
 
 @Component({
   selector: 'app-levels',
@@ -20,5 +21,19 @@ import { LevelService } from '../../services/level/level.service';
 export class LevelsComponent {
   private readonly levelService = inject(LevelService);
   protected levels = this.levelService.levels;
-  protected currentLevel = this.levelService.currentLevel;
+  protected levelIndex = this.levelService.currentLevel;
+
+  private readonly roundService = inject(RoundService);
+  protected rounds = this.roundService.rounds;
+  protected roundIndex = this.roundService.roundIndex;
+
+  private readonly cdr = inject(ChangeDetectorRef);
+
+  constructor() {
+    this.roundService.round.subscribe(() => {
+      this.roundIndex = this.roundService.roundIndex;
+      this.rounds = this.roundService.rounds;
+      this.cdr.detectChanges();
+    });
+  }
 }
