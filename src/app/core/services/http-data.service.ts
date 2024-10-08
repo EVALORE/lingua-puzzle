8fb5807145b4fcdd56b3b7367f6e1fe8c69fbf6f
Vladimir Eremia <caioshi212@gmail.com>
2024-10-08 20:12:35 +0300

    feat(http-data): implement new url data handlings

--- src/app/core/services/http-data.service.ts
+++ src/app/core/services/http-data.service.ts
@@ -1,21 +1,39 @@
-import { Round, Level } from '../../shared/types/http-data.interface';
+import { Level } from '../../shared/types/http-data.interface';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
-import { LevelService } from '../../lingua-puzzle/services/level/level.service';
+import { Observable } from 'rxjs';
+
+const baseLevelUrl = 'https://evalore.github.io/rss-puzzle-data/data/wordCollectionLevel';
+const baseImageUrl = 'https://evalore.github.io/rss-puzzle-data/images';
+const baseAudioUrl = 'https://evalore.github.io/rss-puzzle-data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpDataService {
   private readonly httpClient = inject(HttpClient);
-  private readonly levelService = inject(LevelService);
 
-  private getData(): Observable<Level> {
-    return this.httpClient.get<Level>(this.levelService.levelUrl);
+  public getLevel(levelNumber: number): Observable<Level> {
+    return this.httpClient.get<Level>(this.getLevelUrl(levelNumber));
   }
 
-  public getRounds(): Observable<Round[]> {
-    return this.getData().pipe(map((level) => level.rounds));
+  public getImage(imagePath: string): string {
+    return this.getImageUrl(imagePath);
+  }
+
+  public getAudio(audioPath: string): string {
+    return this.getAudioUrl(audioPath);
+  }
+
+  private getLevelUrl(levelNumber: number): string {
+    return `${baseLevelUrl}/${String(levelNumber)}.json`;
+  }
+
+  private getImageUrl(imagePath: string): string {
+    return `${baseImageUrl}/${imagePath}`;
+  }
+
+  private getAudioUrl(audioPath: string): string {
+    return `${baseAudioUrl}/${audioPath}`;
   }
 }
