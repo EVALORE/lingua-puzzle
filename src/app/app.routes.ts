5c21bc062a68c2c343daf7b373100e46d2e8b616
Vladimir Eremia <vladimireremia377@gmail.com>
2025-02-18 02:35:39 +0200

    Add skipAuthGuard to handle unauthenticated user logic
    Introduced the `skipAuthGuard` to prevent authenticated users from accessing the authentication route. The guard checks the user's login status using `LocalStorageService` and redirects logged-in users to the root path. Included unit tests to ensure the guard's correctness and updated route configurations accordingly.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { isUserGuard } from './core/guards/isUser/is-user.guard';
+import { isUserGuard } from './core/guards/is-user/is-user.guard';
+import { skipAuthGuard } from './core/guards/skip-auth/skip-auth.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
     loadComponent: () => import('./auth/auth.component').then((c) => c.AuthComponent),
+    canMatch: [skipAuthGuard],
   },
   {
     path: '',
