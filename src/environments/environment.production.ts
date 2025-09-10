ce702d5b69e08a6fa351786059348bfbb8923c57
Vladimir Eremia <vladimireremia377@gmail.com>
2025-09-10 11:02:59 +0300

    Add production environment configuration and update build settings
    Introduced `environment.production.ts` with dynamic folder URLs for levels, audio, and images. Updated `angular.json` to include `baseHref` and `deployUrl` for production builds.

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  levelsFolderUrl: _NGX_ENV_.NG_DATA_FOLDER_URL,
+  audioFolderUrl: _NGX_ENV_.NG_AUDIO_FOLDER_URL,
+  imagesFolderUrl: _NGX_ENV_.NG_IMAGES_FOLDER_URL,
+};
